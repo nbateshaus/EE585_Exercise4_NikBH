20ee03beec7d5769003f23ae005c5aa7b83eae46
Nikolaus Bates-Haus <nbateshaus@gmail.com>
2026-02-10 15:03:37 -0500

    style: apply styler style

--- Main.R
+++ Main.R
@@ -3,30 +3,35 @@
 library(tidyverse)
 library(neon4cast)
 library(lubridate)
-#devtools::install_version("rMR", version = "1.1.0")
+# devtools::install_version("rMR", version = "1.1.0")
 library(rMR)
 
 forecast_date <- Sys.Date()
-noaa_date <- Sys.Date() - days(1)  #Need to use yesterday's NOAA forecast because today's is not available yet
+noaa_date <- Sys.Date() - days(1) # Need to use yesterday's NOAA forecast because today's is not available yet
 
-#Step 0: Define team name and team members 
-team_info <- list(team_name = "air2waterSat_MCD",
-                  team_list = list(list(individualName = list(givenName = "Mike", 
-                                                              surName = "Dietze"),
-                                        organizationName = "Boston University",
-                                        electronicMailAddress = "dietze@bu.edu"))
+# Step 0: Define team name and team members
+team_info <- list(
+  team_name = "air2waterSat_MCD",
+  team_list = list(list(
+    individualName = list(
+      givenName = "Mike",
+      surName = "Dietze"
+    ),
+    organizationName = "Boston University",
+    electronicMailAddress = "dietze@bu.edu"
+  ))
 )
 
 ## Load required functions
-if(file.exists("01_download_data.R"))      source("01_download_data.R")
-if(file.exists("02_calibrate_forecast.R")) source("02_calibrate_forecast.R")
-if(file.exists("03_run_forecast.R"))       source("03_run_forecast.R")
-if(file.exists("04_submit_forecast.R"))    source("04_submit_forecast.R")
+if (file.exists("01_download_data.R")) source("01_download_data.R")
+if (file.exists("02_calibrate_forecast.R")) source("02_calibrate_forecast.R")
+if (file.exists("03_run_forecast.R")) source("03_run_forecast.R")
+if (file.exists("04_submit_forecast.R")) source("04_submit_forecast.R")
 
 ### Step 1: Download Required Data
-target     <- download_targets()       ## Y variables
-site_data  <- download_site_meta()
-target     <- merge_met_past(target)   ## append met data (X) into target file
+target <- download_targets() ## Y variables
+site_data <- download_site_meta()
+target <- merge_met_past(target) ## append met data (X) into target file
 met_future <- download_met_forecast(forecast_date) ## Weather forecast (future X)
 
 ## visual check of data
@@ -35,7 +40,7 @@ ggplot(target, aes(x = temperature, y = air_temperature)) +
   labs(x = "NEON water temperature (C)", y = "NOAA air temperature (C)") +
   facet_wrap(~site_id)
 
-met_future %>% 
+met_future %>%
   ggplot(aes(x = datetime, y = air_temperature, group = parameter)) +
   geom_line() +
   facet_wrap(vars(site_id), scales = "free", ncol = 8)
@@ -44,13 +49,13 @@ met_future %>%
 model <- calibrate_forecast(target)
 
 ### Step 3: Make a forecast into the future
-forecast <- run_forecast(model,met_future,site_data)
+forecast <- run_forecast(model, met_future, site_data)
 
-#Visualize forecast.  Is it reasonable?
-forecast %>% 
+# Visualize forecast.  Is it reasonable?
+forecast %>%
   ggplot(aes(x = datetime, y = prediction, group = parameter)) +
   geom_line() +
   facet_wrap(vars(variable, site_id), scales = "free", ncol = 8)
 
 ### Step 4: Save and submit forecast and metadata
-submit_forecast(forecast,team_info,submit=FALSE)
+submit_forecast(forecast, team_info, submit = FALSE)
